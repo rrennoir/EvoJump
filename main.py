f33bb4d56474fa44fb8e46cb777188a8d1cf55ca
Ryan Rennoir <ryan.rennoir@student.unamur.be>
2019-05-06 22:30:48 +0200

    add player

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ def main():
     # init pygame, import every module in pygame.
     pg.init()
 
-     # Setup window of 500 by 400 pixel.
-    display_size = [500, 400]
+    # Setup window of 500 by 400 pixel.
+    width = 500
+    height = 400
+    display_size = [width, height]
     screen = pg.display.set_mode(display_size)
 
     # Set the windows title
@@ -28,11 +30,17 @@ def main():
                 pg.quit()
                 quit()
 
-    screen.blit(screen, (0, 0))
-    pg.display.flip()
 
-    # Set the game to 60 update per second.
-    clock.tick(60)
+        # Draw player on the middle of the screen.
+        player_size = 20
+        player_rect = pg.Rect(width / 2, height / 2, player_size, player_size)
+        pg.draw.rect(screen, (255, 255, 255), player_rect)
+
+        screen.blit(screen, (0, 0))
+        pg.display.flip()
+
+        # Set the game to 60 update per second.
+        clock.tick(60)
 
 
 if __name__ == "__main__":
