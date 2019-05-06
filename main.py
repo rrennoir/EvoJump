9d25471957b4f9304faa3ed44680bc49370a3ce6
Ryan Rennoir <ryan.rennoir@student.unamur.be>
2019-05-06 22:40:34 +0200

    add obstacle (spawn + update)

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ def main():
 
     clock = pg.time.Clock()
 
+    obstacle_rect_list = []
+    tick = 0
     play = True
     while play:
 
@@ -30,14 +32,33 @@ def main():
                 pg.quit()
                 quit()
 
+        # Spawn a obstacle every seconde just out side of the screen.
+        if tick == 60:
+            obstacle_rect = pg.Rect(width, height / 2, 20, 20)
+            obstacle_rect_list.append(obstacle_rect)
+
+        # Update obstacle position.
+        for index, obstacle_rect in enumerate(obstacle_rect_list):
+            obstacle_rect_list[index] = obstacle_rect.move(-1, 0)
 
         # Draw player on the middle of the screen.
         player_size = 20
         player_rect = pg.Rect(width / 2, height / 2, player_size, player_size)
         pg.draw.rect(screen, (255, 255, 255), player_rect)
 
+        # Draw obstacle.
+        for obstacle in obstacle_rect_list:
+            pg.draw.rect(screen, (255, 0, 0), obstacle)
+
+        # Update display.
         screen.blit(screen, (0, 0))
         pg.display.flip()
+
+        # Count tick.
+        if tick > 60:
+            tick = 0
+
+        tick += 1
 
         # Set the game to 60 update per second.
         clock.tick(60)
